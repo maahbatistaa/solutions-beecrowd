08094228722af68b6b3359138fecbe5019bf4020
Mariana Batista <104997176+maahbatistaa@users.noreply.github.com>
2025-03-25 09:31:14 -0300

    1002 and 1003 in C#

---
+++ iniciante/csharp/b1002.cs
@@ -0,0 +1,6 @@
+double n, r;
+
+n = double.Parse(Console.ReadLine());
+r = 3.14159 * (n * n);
+
+Console.WriteLine("A=" + r.ToString("F4"));
---
+++ iniciante/csharp/b1003.cs
@@ -0,0 +1,8 @@
+int A, B, SOMA;
+
+A = int.Parse(Console.ReadLine());
+B = int.Parse(Console.ReadLine());
+
+SOMA = A + B;
+
+Console.WriteLine("SOMA = " + SOMA);
